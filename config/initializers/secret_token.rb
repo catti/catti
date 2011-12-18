ccbe4d548ebf0cfbd86c7f0ed871e293b079e616
Kati Norrena <kati.norrena@eficode.fi>
2011-12-18 19:46:06 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catti::Application.config.secret_token = 'ccca58bc4394c7b632c1cf47fbad155ef5f0fa437be652371a7345604c650bbbfa547b4ef9805c6dc4b41082d8c5d9c0c671072edc55df6005978ffc31339176'
